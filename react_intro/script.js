3e0535eb586bdc30f50137e517369dca51199911
unknown <ahmadmurtazazahid@gmail.com>
2023-08-29 12:39:48 +0200

    update

---
+++ react_intro/script.js
@@ -0,0 +1,21 @@
+function MyButton() {
+    return (
+      <button>I'm a button</button>
+    );
+  }
+  export default function MyApp() {
+    return (
+      <div>
+        <h1>React Intro</h1>
+        <MyButton />
+      </div>
+    );
+  }
+  function AboutPage() {
+    return (
+      <>
+        <h1>About</h1>
+        <p>Hello there.<br />How do you do?</p>
+      </>
+    );
+  }
