28db75b76a27e9b5ea218eb09cc1f4c9789b813c
unknown <ahmadmurtazazahid@gmail.com>
2023-08-31 11:30:31 +0200

    update

---
+++ react_intro/update/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'> 
+    <h1>Liste</h1>
+    
+  
+ 
+ <ul>
+<li> Oppgave 1 </li>
+<li> A </li>
+<li>Hva skriver du i javascript dersom du vil gi variable test verdien 8?</li>
+<li> vanlig <b></b></li>
+</ul>
+   
+
+    </div>
+   
+  );
+}
+
+export default App;
