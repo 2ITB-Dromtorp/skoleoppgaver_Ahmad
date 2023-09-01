734aabe7905a00705d9e44caec24d04c86569792
unknown <ahmadmurtazazahid@gmail.com>
2023-09-01 09:40:40 +0200

    oppdatering

--- react_intro/update/src/App.js
+++ react_intro/update/src/App.js
@@ -12,7 +12,12 @@ function App() {
 <li> Oppgave 1 </li>
 <li> A </li>
 <li>Hva skriver du i javascript dersom du vil gi variable test verdien 8?</li>
-<li> vanlig <b></b></li>
+<li>  <b>let test 8 </b></li>
+<li>B</li>
+<li>Hva skriver du dersom du vil gi variabelen test verdien "testverdi"? Hvilken datatype er det her</li>
+<li> <b> let test = "testverdi"</b></li>
+<li>Hva skriver du dersm du vil renge ut 2*3 og sette resultatet inn i variabelen produkt?</li>
+<li> <b>Return function</b></li>
 </ul>
    
 
