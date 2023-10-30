5acfa8735b97436a80a001cac71798140cdd6113
unknown <ahmadmurtazazahid@gmail.com>
2023-10-30 14:06:27 +0100

    shuffle

--- react_intro/klassekartt/src/home.js
+++ react_intro/klassekartt/src/home.js
@@ -1,63 +1,92 @@
+import { useState } from 'react';
 import './App.css';
 import Elev from './Elev';
 
 export default function Home() {
-
+    const [names, setNames] = useState([
+        "Andreas",
+        "Ahmad",
+        "Philip",
+        "Gabriel",
+        "Theodor",
+        "Mattis",
+        "Alva",
+        "Silas",
+        "Axel",
+        "Vetle",
+        "Kristoffer",
+        "Johannes",
+        "Elias",
+        "Matheo",
+    ]);
+    
+    
+    function shuffle() {
+        const newNames = [...names];
+        for (let i = names.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            const newN = newNames[i];
+            const oldN = newNames[j];
+            newNames[i] = oldN;
+            newNames[j] = newN;
+        }
+        setNames(newNames);
+    }
     return (
+        <>
+            <button onClick={shuffle}>trykk</button>
+            <div className="container">
 
-        <div className="container">
+                <div className='leftside'>
 
-        <div className='leftside'>
+                    <div className='box'>
 
-            <div className='box'>
+                        <div className='sitteplasser'>
+                            <Elev name={names[0]} />
+                            <Elev name={names[1]} />
+                        </div>
 
-              <div className='sitteplasser'>
-                <Elev name="Andreas"/>
-                <Elev name="Ahmad"/>
-              </div>
+                        <div className='sitteplasser'>
+                            <Elev name={names[2]} />
+                        </div>
 
-              <div className='sitteplasser'> 
-                <Elev name="Philip"/>
-              </div>
+                        <div className='sitteplasser'>
+                            <Elev name={names[3]} />
+                            <Elev name={names[4]} />
+                        </div>
 
-              <div className='sitteplasser'> 
-                <Elev name="Gabriel"/>
-                <Elev name="Theodor"/>
-              </div>
+                    </div>
+                </div>
+
+
+                <div className='rightside'>
+
+                    <div className='box'>
+
+                        <div className='sitteplasser'>
+                            <Elev name={names[5]} />
+                            <Elev name={names[6]} />
+                            <Elev name={names[7]} />
+                        </div>
+
+                        <div className='sitteplasser'>
+                            <Elev name={names[8]} />
+                            <Elev name={names[9]} />
+                            <Elev name={names[10]} />
+                        </div>
+
+                        <div className='sitteplasser'>
+                            <Elev name={names[11]} />
+                            <Elev name={names[12]} />
+                            <Elev name={names[13]} />
+                        </div>
+
+                    </div>
+
+                </div>
+
 
             </div>
-          </div>
-            
-
-        <div className='rightside'>
-
-        <div className='box'>
-
-            <div className='sitteplasser'> 
-                <Elev name="Mattis"/>
-                <Elev name="Alva"/>
-                <Elev name="Silas"/>
-              </div>
-
-            <div className='sitteplasser'> 
-                <Elev name="Axel"/>
-                <Elev name="Vetle"/>
-                <Elev name="Kristoffer"/>
-              </div>
-
-              <div className='sitteplasser'> 
-                <Elev name="Johannes"/>
-                <Elev name="Elias"/>
-                <Elev name="Matheo"/>
-              </div>
-            
-          </div>
-
-          </div>
-
-
-        </div>
-    )
+        </>
+    );
 }
-
-
