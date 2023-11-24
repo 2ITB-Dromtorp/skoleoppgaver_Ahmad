441d6558a004896ddfe5e7c2286ac9f06143517e
unknown <ahmadmurtazazahid@gmail.com>
2023-11-24 13:25:08 +0100

    rah

--- react_intro/raaaaaaaaaaaaaaaaaaah/src/pages/kurs.js
+++ react_intro/raaaaaaaaaaaaaaaaaaah/src/pages/kurs.js
@@ -6,14 +6,14 @@ const alleKurs = {
            
            
            <p id="gymside" >
-                Her pumper vi jern
+                
 
-                <img id="mentzer" src="https://thebarbell.com/wp-content/uploads/2023/02/Mike-Mentzer-workout-4.jpg" alt="arnold"/>
-
+                <h1 id="mentzer"> Kroppsøving er mye mer enn bare fysisk aktivitet – det er en utforskning av kroppens potensial, bevegelsesmønstre og helse. I dette kurset vil vi utforske hvordan kroppen fungerer og utvikler, seg gjennom ulike former for trening og aktivitet. Vi vil se på både teoretiske og praktiske aspekter av kroppsøving, inkludert anatomi, idrettslige ferdigheter, helsefordeler og metoder for å  </h1>
 
                 
-    
-            </p>
+                <img id="mentzer" src="https://pngimg.com/d/dumbbell_PNG16376.png" alt="arnold"/>
+                 </p>
+                
                 
                 
                 
@@ -33,14 +33,41 @@ const alleKurs = {
     heimkunnskap: {
         innhold: (
             <>
-               bak brød eller no
+               <p id="heim"> 
+Heimkunskap, også kjent som husholdningskunnskap eller hjemmeøkonomi, er et fagområde som fokuserer på praktiske ferdigheter og kunnskap som er viktige for å kunne håndtere ulike aspekter av dagliglivet. Dette kan inkludere alt fra matlaging, renhold og personlig økonomi til husstell, klespleie og ernæring.
+
+En introduksjon til heimkunskap kan inneholde:
+
+Matlaging: Grunnleggende matlagingsferdigheter, forståelse av ulike matvarer, kostholdsinformasjon og sunne matvaner.
+
+Husstell: Rengjøringsteknikker, organisering av hjemmet, vedlikehold av møbler og generell husholdningspleie.
+
+.</p>
+
+<p id="hei"> Personlig økonomi: Budsjettlegging, sparing, forståelse av økonomiske begreper og ansvarlig pengebruk.
+
+Klespleie: Vasking, stryking, enkel reparasjon av klær og forståelse av materialer.
+
+Ernæring: Grunnleggende kunnskap om næringsstoffer, viktigheten av balansert kosthold og hvordan lage sunne måltider.
+
+En heimkunskapskurs kan være praktisk rettet, med fokus på hands-on øvelser og demonstrasjoner for å lære disse ferdighetene. Det kan også inkludere teoretisk kunnskap om forskjellige emner relatert til hjemmeøkonomi.
+
+Det er ofte en nyttig ressurs for alle, uavhengig av alder eller erfaring, da det gir praktiske ferdigheter som er viktige for å kunne leve selvstendig og effektivt.  </p>
+
+
+<img id="mat" src="https://d2rdhxfof4qmbb.cloudfront.net/wp-content/uploads/20180323163421/Ramen.jpg" alt="mat"/>
+
+
+
+               
             </>
         ),
     },
     datakunnskap: {
         innhold: (
             <>
-                lær hacking
+                <img id="retro" src="https://i.pinimg.com/originals/83/d5/e0/83d5e040af429fd72ba7b57924fd3d3b.jpg" alt="pc"/>
+
             </>
         ),
     },
