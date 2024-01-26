dbd4352d025d007734e4669eec8c7772c1d64a1f
Ahmad Murtaza Zahid <ahmadmurtazazahid@gmail.com>
2024-01-26 10:22:36 +0100

    :airplane::collision::hospital::hospital::exploding_head:

--- reactfire/src/App.js
+++ reactfire/src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./login";
 import Home from "./Home";
- 
+
 export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route index element={<Home />} />
+                <Route path="/login" element={<Login />} />
+            </Routes>
+        </Router>
+    );
 }
